d5cc10ec9017804822dc46eaca94619e63a8c7c6
joyqi <magike.net@gmail.com>
2022-10-24 00:22:01 +0800

    fix node replace

--- src/component/init.ts
+++ src/component/init.ts
@@ -7,17 +7,16 @@ type TypedFunctionComponent<T> = (props: T, children?: VNode[]) => VNode;
 type NulledFunctionComponent = (props: NullParams, children?: VNode[]) => VNode;
 
 export function initComponent(modules?: Module[]) {
-    let defaultPatch = modules ? init(modules) : null;
-
     return <T extends State>(fn: TypedFunctionComponent<T>, params: T): [NulledFunctionComponent, T] => {
         const state = createState(params);
 
         return [(_: NullParams, children?: VNode[]): VNode => {
             let vnode = fn(state, children);
-            const patch = defaultPatch || init(detectModules(vnode));
+            const patch = init(modules || detectModules(vnode), undefined, { experimental: { fragments: true } });
 
             ref(state).effect(() => {
-                vnode = patch(vnode, fn(state, children));
+                const newVnode = patch(vnode, fn(state, children));
+                Object.assign(vnode, newVnode);
             });
 
             return vnode;
