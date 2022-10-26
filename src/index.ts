b57b741b529370fc195401ed6f9bf11a8888dd1d
joyqi <magike.net@gmail.com>
2022-10-26 13:58:41 +0800

    fix export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { $, htmlVNode, patchDom, detectModules } from './helpers';
+export { $, htmlVNode, detectModules } from './helpers';
 
 export { initComponent } from './component/init';
 export { ref } from './component/state';
