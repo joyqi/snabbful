d2a65c2b00fa6a0ed9d75642b9658dde4abed76d
joyqi <magike.net@gmail.com>
2022-11-09 02:00:43 +0800

    fix modules

--- src/component/init.ts
+++ src/component/init.ts
@@ -1,18 +1,17 @@
 import { VNode, init, Module } from 'snabbdom';
-import { detectModules } from '../helpers';
 import { createState, State, ref } from './state';
 
 type NullParams = {};
 type TypedFunctionComponent<T> = (props: T, children?: VNode[]) => VNode;
 type NulledFunctionComponent = (props: NullParams, children?: VNode[]) => VNode;
 
-export function initComponent(modules?: Module[]) {
+export function initComponent(modules: Module[]) {
     return <T extends State>(fn: TypedFunctionComponent<T>, params: T): [NulledFunctionComponent, T] => {
         const state = createState(params);
 
         return [(_: NullParams, children?: VNode[]): VNode => {
             let vnode = fn(state, children);
-            const patch = init(modules || detectModules(vnode), undefined, { experimental: { fragments: true } });
+            const patch = init(modules, undefined, { experimental: { fragments: true } });
 
             ref(state).watch(() => {
                 const newVnode = patch(vnode, fn(state, children));
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,2 @@
-export { htmlVNode, detectModules } from './helpers';
-
 export { initComponent } from './component/init';
 export { ref } from './component/state';
